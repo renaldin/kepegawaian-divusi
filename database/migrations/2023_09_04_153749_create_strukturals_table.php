312f69181b4fe118dcd48e3d184539cc99465a39
renaldin <renaldinoviandi0@gmail.com>
2023-09-04 08:27:53 +0700

    Migrations 04-09-2023

---
+++ database/migrations/2023_09_04_153749_create_strukturals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStrukturalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('struktural', function (Blueprint $table) {
+            $table->bigIncrements('id_struktural');
+            $table->string('nama_struktural');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('struktural');
+    }
+}
---
+++ database/migrations/2023_09_04_153816_create_presensis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePresensisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presensi', function (Blueprint $table) {
+            $table->bigIncrements('id_presensi');
+            $table->integer('id_karyawan')->nullable();
+            $table->enum('jenis_presensi',['hadir','sakit','izin','alfa'])->nullable();
+            $table->string('keterangan')->nullable();
+            $table->date('tanggal')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presensi');
+    }
+}
